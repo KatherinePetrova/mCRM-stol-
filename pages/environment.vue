575717c904b3d090c1b057f64bb832e3c3a90de5
KatherinePetrova <zetsu9@mail.ru>
2019-01-08 11:31:54 +0600

    first commit

---
+++ pages/environment.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="main">
+		<nuxt-link to="/" style="margin-top: 20px">Перейти</nuxt-link>
+	</div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style scoped>
+	.main {
+		display: flex;
+		min-height: 100vh;
+		min-width: 100vw;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		flex-direction: column;
+	}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="main">
+		<logo></logo>
+		<nuxt-link to="/environment" style="margin-top: 20px">Перейти</nuxt-link>
+	</div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style scoped>
+	.main {
+		display: flex;
+		min-height: 100vh;
+		min-width: 100vw;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		flex-direction: column;
+	}
+</style>
